f7e7d7032aa4d71744b963947fbb27c47451492c
Yaroslav Khoruzhenko <yaroslav.khoruzhenko@cds-global.com>
2023-06-09 12:53:42 +0300

    enforce FK constraint

--- app/blog/database.py
+++ app/blog/database.py
@@ -1,10 +1,12 @@
 import os
+from sqlite3 import Connection
 from typing import Iterator
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.pool.base import _ConnectionRecord
 
 env = os.getenv('env', 'prod')
 DATABASE_URL = 'sqlite:///./blog.db'
@@ -12,6 +14,12 @@ TEST_DATABASE_URL = 'sqlite:///./blog_test.db'
 engine = create_engine(DATABASE_URL if env == 'prod' else TEST_DATABASE_URL,
                        connect_args={"check_same_thread": False})
 
+
+def _fk_pragma_on_connect(dbapi_con: Connection, con_record: _ConnectionRecord) -> None:
+    dbapi_con.execute('pragma foreign_keys=ON')
+
+
+event.listen(engine, 'connect', _fk_pragma_on_connect)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
